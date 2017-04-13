ecbe9fb57f1e8d3917b70aef3a79ca21e4099e38
Dario Calderone <darkal95@libero.it>
2017-04-13 11:31:28 +0200

    terra di mezzo

---
+++ HelloMiddleHeart/src/it/unirma3/git/HelloMiddleHeart.java
@@ -0,0 +1,15 @@
+package it.unirma3.git;
+
+public class HelloMiddleHeart {
+	
+	public static void main(String[] args) {
+		
+		String[] names = {"Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf" };
+		
+		//stampa i saluti
+		for (int i = 0; i < names.length; i++){
+			System.out.println("Hello, " + names[i] + "\n");
+		}
+	}
+
+}
